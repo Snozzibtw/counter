6e6903a4738f3b96f387c26a8406a0b80d2c5401
Snozzi <99423719+Snozzibtw@users.noreply.github.com>
2025-04-29 01:53:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let countEl = document.getElementById("count")
+let reduceEl = document.getElementById("reduce")
 let count = 0
 let saveEl = document.getElementById("save-el")
 let resetEl = document.getElementById("reset")
 
 function increment() {
     count += 1
+    countEl.textContent = count
+}
+
+function reduction() {
+    count -= 1
     countEl.textContent = count
 }
 
