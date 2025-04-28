c7a9bd64dcbccf1bf99bfc29b923a7fdedffaf70
Snozzi <99423719+Snozzibtw@users.noreply.github.com>
2025-04-29 01:30:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let countEl = document.getElementById("count")
+let count = 0
+let saveEl = document.getElementById("save-el")
+let resetEl = document.getElementById("reset")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countSeperator = count + " - "
+    saveEl.textContent += countSeperator
+    console.log(count)
+}
+
+function reset() {
+    countEl.textContent = 0
+    count = 0
+}
